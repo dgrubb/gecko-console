c53e9b2fb3cd5eb6f8b6e48c67f791d21b98b5a7
dgrubb <davidanthonygrubb@gmail.com>
2019-05-13 10:17:00 -0500

    Add personalities directory with sample game JSON
    * Add Atari Jaguar personlity profile.

---
+++ gecko/personalities/jaguar.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: "Atari Jaguar",
+    gameList: [
+        {
+            name: "Tempest 2000",
+            releaseDate: "1994",
+            launchCommand: "virtualjaguar",
+            launchArguments: [
+                "-g"
+            ],
+            romPath: "./roms/tempest",
+            boxArt: "./assets/tempest_box.png"
+        }
+    ]
+};
